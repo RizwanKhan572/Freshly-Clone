ee8ae475f657851a7d73fe219dbac7b1b5df4d13
Rizwan <imriso143@gmail.com>
2022-06-17 14:59:35 +0530

    fresly

---
+++ Scripts/login.js
@@ -0,0 +1,5 @@
+
+
+document.getElementById("nav_btn").onclick=function() {
+    window.location.href="signup.html"
+}
---
+++ Scripts/main.js
@@ -0,0 +1,29 @@
+
+
+function click_me () {
+    window.location.href="signup.html"
+}
+
+document.getElementById('plans').onclick=function() {
+    window.location.href="https://www.freshly.com/plans-and-menu"
+}
+
+
+document.getElementById('howit').onclick=function() {
+    window.location.href="https://www.freshly.com/why-freshly"
+}
+
+
+document.getElementById('gifts').onclick=function() {
+    window.location.href="https://www.freshly.com/gift-cards"
+}
+
+
+document.getElementById('refer').onclick=function() {
+    window.location.href=""
+}
+
+
+document.getElementById('help').onclick=function() {
+    window.location.href="https://support.freshly.com/hc/en-us"
+}
